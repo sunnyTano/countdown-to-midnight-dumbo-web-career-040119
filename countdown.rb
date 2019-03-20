a7cc0ea826a76b3704d6a8f3a7dec8df575d053b
Susan Tan <ssusantan9@gmail.com>
2019-03-20 05:20:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-def countdown(number)
-  while number > 0 
-    puts `#{number} SECOND(S)!`
-    number -= 1
+def countdown(num)
+  while num > 0 
+    puts `#{num} SECOND(S)!`
+    num -= 1
    end
   return "HAPPY NEW YEAR!"
 end
