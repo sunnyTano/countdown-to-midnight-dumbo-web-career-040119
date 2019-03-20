a03a02931fd7ff3fbc6c47891d7301fc18555762
Susan Tan <ssusantan9@gmail.com>
2019-03-20 05:13:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,11 +6,11 @@ def countdown(num)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown(num)
+def countdown_with_sleep(num)
   while num > 0 
-    puts `#{num} SECOND(S)!`
-    num -= 1
-      sleep(1)
-   end
+   puts `#{num} SECOND(S)` 
+     sleep(1)
+   num -= 1 
+  end
   return "HAPPY NEW YEAR!"
 end
