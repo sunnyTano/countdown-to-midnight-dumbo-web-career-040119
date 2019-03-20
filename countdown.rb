9866e7033d852e4fee27def631696d748e79d46e
Susan Tan <ssusantan9@gmail.com>
2019-03-20 05:22:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,19 @@
-# def countdown(num)
-#   while num > 0 
-#     puts `#{num} SECOND(S)!`
-#     num -= 1
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
+def countdown(num)
+  while num > 0 
+    puts `#{num} SECOND(S)!`
+    num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-# def countdown_with_sleep(num)
-#   while num > 0
-#     sleep 1
-#     puts "#{num} SECOND(S)!"
-#     num -= 1
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
+def countdown_with_sleep(num)
+  while num > 0
+    sleep 1
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
 def countdown(seconds)
   while seconds > 0
